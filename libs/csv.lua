f3831cfa44aa0ae8d818d2be7e0ef240d86bb85d
Stein-Ivar Berghei <stein-ivar@berghei.no>
2012-02-21 23:24:07 +0100

    Add the csv library

---
+++ libs/csv.lua
@@ -0,0 +1,15 @@
+local field =
+	lpeg.P(' ')^0
+	* '"' * lpeg.Cs(((lpeg.P(1) - '"') + lpeg.P'""' / '"')^0) * '"'
+	* lpeg.P(' ')^0
+	+ lpeg.C((1 - lpeg.S',\t\n"')^0)
+
+local record =
+	lpeg.Ct(field * ((lpeg.P(',') + lpeg.P('\t')) * field)^0)
+	* (lpeg.P'\n' + -1)
+
+local csv = function(s)
+	 return lpeg.match(record, s)
+end
+
+return csv
