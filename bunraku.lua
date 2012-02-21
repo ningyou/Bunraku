d6d3acafdd2f35b4a2305eb8ba8d972d38c8038c
Stein-Ivar Berghei <stein-ivar@berghei.no>
2012-02-21 23:22:39 +0100

    Use lua-ev for the loop and move the csv bit into its own library for now.

--- bunraku.lua
+++ bunraku.lua
@@ -9,32 +9,24 @@ local zmq = require"zmq"
 local lpeg = require"lpeg"
 local mongo = require"mongo"
 local xpath = require"xpath"
-
+local ev = require"ev"
+local loop = ev.Loop.default
+local csv = require"csv"
 local db = mongo.Connection.New()
 db:connect"localhost"
 
-
-local field =
-	lpeg.P(' ')^0
-	* '"' * lpeg.Cs(((lpeg.P(1) - '"') + lpeg.P'""' / '"')^0) * '"'
-	* lpeg.P(' ')^0
-	+ lpeg.C((1 - lpeg.S',\t\n"')^0)
-
-local record =
-	lpeg.Ct(field * ((lpeg.P(',') + lpeg.P('\t')) * field)^0)
-	* (lpeg.P'\n' + -1)
-
-local csv = function(s)
-	 return lpeg.match(record, s)
-end
-
-local fetch = function(type, title)
-	-- Placeholder stub
-	return
-end
-
 local trim = function(s)
 	return s:match('^()%s*$') and '' or s:match('^%s*(.*%S)')
+end
+
+local handle_msg = function(data)
+	local parse = csv(data)
+	if parse then
+		local type = parse[1]
+		for i = 2, #parse do
+			print(type, trim(parse[i]))
+		end
+	end
 end
 
 local ctx = zmq.init(1)
@@ -42,13 +34,24 @@ local s = ctx:socket(zmq.SUB)
 s:setopt(zmq.SUBSCRIBE, "")
 s:bind"ipc:///tmp/bunraku.sock"
 
-while true do
-	local recv_csv = s:recv()
-	local parse = csv(recv_csv)
-	if parse then
-		local type = parse[1]
-		for i = 2, #parse do
-			fetch(type, parse[i])
-		end
+local s_io_idle
+local s_io_read
+
+s_io_idle = ev.Idle.new(function()
+	local msg, err = s:recv(zmq.NOBLOCK)
+	if err == "timeout" then
+		s_io_idle:stop(loop)
+		s_io_read:start(loop)
+		return
 	end
-end
+	handle_msg(msg)
+end)
+
+s_io_idle:start(loop)
+
+s_io_read = ev.IO.new(function()
+	s_io_idle:start(loop)
+	s_io_read:stop(loop)
+end, s:getopt(zmq.FD), ev.READ)
+
+loop:loop()
