741e32794884accbc75d966dc136e0f45ba08642
Stein-Ivar Berghei <stein-ivar@berghei.no>
2012-03-04 17:37:04 +0100

    Check the existing cache before fetching, also rename aid to id and anidbkey to key

--- modules/anidb.lua
+++ modules/anidb.lua
@@ -32,14 +32,14 @@ function _M:Queue(data, force)
 	end
 	
 	for i = 2, #data do
-		local aid = tonumber(data[i])
-		local key = "anidb:"..aid
+		local id = tonumber(data[i])
+		local key = "anidb:"..id
 		if cache:exists(key) and (cache:ttl(key) > 86400 or cache:ttl(key) == -1) and not force then
 			bunraku:Log('info', 'Cache already exists for: %s.', cache:hget(key, 'title'))
 		elseif tableHasValue(_M.queue, id) then
-			bunraku:Log('info', 'Request for AniDB id %s is already queued', aid)
+			bunraku:Log('info', 'Request for AniDB id %s is already queued', id)
 		else
-			table.insert(_M.queue, aid)
+			table.insert(_M.queue, id)
 		end
 	end
 	if _M.queue[1] and not _M.timer:is_active() then
@@ -48,12 +48,17 @@ function _M:Queue(data, force)
 	cache:quit()
 end
 
-function _M:Fetch(aid)
+function _M:Fetch(id)
 	local cache = Redis.connect('127.0.0.1', 6379)
-	local aid = tonumber(aid)
-	local anidbkey = "anidb:"..aid
+	local id = tonumber(id)
+	local key = "anidb:"..id
+	
+	if cache:exists(key) and (cache:ttl(key) > 86400 or cache:ttl(key) == -1) and not force then
+		return bunraku:Log('info', 'Cache already exists for: %s.', cache:hget(key, 'title'))
+	end
+
 	simplehttp(
-		('http://api.anidb.net:9001/httpapi?request=anime&aid=%d&client=bunraku&clientver=1&protover=1'):format(aid),
+		('http://api.anidb.net:9001/httpapi?request=anime&aid=%d&client=bunraku&clientver=1&protover=1'):format(id),
 		function(data)
 			local xml = zlib.inflate() (data)
 			local xml_tree = lom.parse(xml)
@@ -77,13 +82,13 @@ function _M:Fetch(aid)
 			}
 
 			for k,v in next, input do
-				cache:hset(anidbkey, k, v)
+				cache:hset(key, k, v)
 			end
 
 			if not input.enddate then
-				cache:expire(anidbkey, 604800)
+				cache:expire(key, 604800)
 			else
-				cache:persist(anidbkey)
+				cache:persist(key)
 			end
 
 			cache:quit()
